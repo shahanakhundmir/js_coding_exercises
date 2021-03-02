0768ed51cc7b8585fd1aaad6348eb540a9217fa1
shahanakhundmir <58731479+shahanakhundmir@users.noreply.github.com>
2021-03-02 13:07:39 +0000

    third set of exercises completed

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,28 +1,63 @@
 function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
   // Your code here!
+  let squares =[]
+  for (num of nums){
+    squares.push(num*num);
+  }
+  return squares;
 }
 
 function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
   // Your code here!
+  cCWord = '';
+  for (i = 0; i<words.length; i++){
+    if (i === 0){
+      cCWord += words[i];
+    }
+    else{
+      let upperCase = words[i].slice(0,1).toUpperCase() + words[i].slice(1);
+      cCWord += upperCase;
+    }
+  }
+  return cCWord
+  
 }
 
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
+  let total = 0 
+  for ( person of people){
+    total += person.subjects.length ;
+  }
+  return total
 }
 
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
   // Your code here!
+  for(menuItem of menu){
+    if (menuItem.ingredients.includes(ingredient)){
+      return true;
+    }
+  }
+  return false;
 }
 
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
   // Your code here!
+  let result = []
+  for(val of arr1){
+    if (arr2.includes(val) && result.includes(val) === false){
+      result.push(val);
+    }
+  }
+  return result.sort((a, b) => a - b);
 }
 
 module.exports = {
